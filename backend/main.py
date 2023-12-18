8516d8d02cfea7c9d2cbf0b814a0eeedcbd10720
b4zuk <luca.avi20@gmail.com>
2023-12-18 19:25:55 +0100

    Updated missing comments and imports

--- backend/main.py
+++ backend/main.py
@@ -10,6 +10,18 @@ from util import config_logging
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "*"}})
 
+# Here are the imports for the different API services that will be loaded in by flask automaticly
+from services import setup_device
+from services import handle_device
+from services import handle_group
+from services import handle_actuator
+from services import handle_threshold
+from services import handle_measurement
+
+
+# TODO: Implement 422 status code if varibales are missing in a POST-request
+# TODO: Implement flag option to run the server in a different logging mode and also provide option to reset the DB
+
 
 if __name__ == '__main__':
     database.setup_database("smart_gardening_db.db")
